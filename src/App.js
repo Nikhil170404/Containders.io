32ddd192fab85f66895604bc831602d99967838d
Nikhil170404 <prashants1704@gmail.com>
2024-12-28 23:20:42 +0530

    Updated configuration for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ function App() {
     <ThemeProvider theme={gameTheme}>
       <CssBaseline />
       <NotificationProvider>
-        <Router basename="/Containders.io">
+        <Router basename="/your-github-repo-name">
           <NotificationPermission />
           <Box sx={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}>
             <Navbar />
